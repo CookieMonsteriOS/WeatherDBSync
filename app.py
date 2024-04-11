c374a2163cc271b4f51b26889cb0f21e955de607
Sam Chaudry <sam.chaudry@hotmail.co.uk>
2024-04-11 14:32:06 +0100

    basic weather end point up and running

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, request
+from database.db import database
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = '' #Replace with your details
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+database.init_app(app)
 
 @app.route("/")
-def hello_world():
+def index():
     return "Hello, World!"
+
+@app.route('/weather', methods=['POST'])
+def get_weather():
+    data = request.get_json()
+    lat = data.get('lat')
+    long = data.get('long')
+    city = data.get('city')
+    return {"response":"Some inputs recieved"},200
 
 if __name__ == '__main__':
     app.run(debug=True)
